3a2ee7d1129f7113e2263ad94402a926ecd57eee
juliet <juliet.zhu.work@gmail.com>
2014-05-23 23:18:13 +0800

    solve conflict.

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,15 +67,22 @@ module.exports = function (grunt) {
             options: {
                 port: 9000,
                 open: true,
-                livereload: 35729,
+                 livereload: 35729,
                 // Change this to '0.0.0.0' to access the server from outside
-                //hostname: '0.0.0.0'
+                hostname: '0.0.0.0'
             },
             livereload: {
                 options: {
                     base:'dev',
                     middleware: function(connect, options, middlewares) {
-                            middlewares.unshift(require('connect-livereload')());
+                            middlewares.push(
+                                function (req,res,next){
+                                
+                                    console.log('================');
+                                    console.log('hehe')
+                                    console.log('================');
+                                
+                            });
                             return middlewares;
                     }
                     
